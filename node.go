d711dad1b180d9e329920267a83b9a5014af83bb
pchchv <ipchchv@gmail.com>
2025-04-29 19:35:26 +0300

    Implement check method for existingParams

--- node.go
+++ node.go
@@ -9,3 +9,11 @@ const (
 type nodeType uint8
 
 type existingParams map[string]struct{}
+
+func (e existingParams) check(param string, path string) {
+	if _, ok := e[param]; ok {
+		panic("duplicate param name '" + param + "' detected for route '" + path + "'")
+	}
+
+	e[param] = struct{}{}
+}
