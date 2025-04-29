dc25e5afaa23180e1d8c70601af7aa237481d526
pchchv <ipchchv@gmail.com>
2025-04-29 19:43:23 +0300

    Implement insertChild method for node

--- node.go
+++ node.go
@@ -29,3 +29,110 @@ type node struct {
 	nType     nodeType
 	wildChild bool
 }
+
+func (n *node) insertChild(numParams uint8, existing existingParams, path string, fullPath string, handler http.HandlerFunc) {
+	var offset int // already handled bytes of the path
+	// find prefix until first wildcard
+	// (beginning with paramByte' or wildByte')
+	for i, max := 0, len(path); numParams > 0; i++ {
+		c := path[i]
+		if c != paramByte && c != wildByte {
+			continue
+		}
+
+		// find wildcard end
+		// (either '/' or path end)
+		end := i + 1
+		for end < max && path[end] != slashByte {
+			switch path[end] {
+			// wildcard name must not contain ':' and '*'
+			case paramByte, wildByte:
+				panic("only one wildcard per path segment is allowed, has: '" + path[i:] + "' in path '" + fullPath + "'")
+			default:
+				end++
+			}
+		}
+
+		// check if this node existing children,
+		// which will be unreachable if a wildcard is inserted here
+		if len(n.children) > 0 {
+			panic("wildcard route '" + path[i:end] + "' conflicts with existing children in path '" + fullPath + "'")
+		}
+
+		if c == paramByte { // param
+			// check if the wildcard has a name
+			if end-i < 2 {
+				panic("wildcards must be named with a non-empty name in path '" + fullPath + "'")
+			}
+
+			// split path at the beginning of the wildcard
+			if i > 0 {
+				n.path = path[offset:i]
+				offset = i
+			}
+
+			child := &node{
+				nType: hasParams,
+			}
+			n.children = []*node{child}
+			n.wildChild = true
+			n = child
+			n.priority++
+			numParams--
+			// if the path doesn't end with the wildcard,
+			// then there will be another non-wildcard subpath starting with '/'
+			if end < max {
+				existing.check(path[offset:end], fullPath)
+				n.path = path[offset:end]
+				offset = end
+				child := &node{
+					priority: 1,
+				}
+				n.children = []*node{child}
+				n = child
+			}
+		} else { // catchAll
+			if end != max || numParams > 1 {
+				panic("character after the * symbol is not permitted, path '" + fullPath + "'")
+			}
+
+			if len(n.path) > 0 && n.path[len(n.path)-1] == '/' {
+				panic("catch-all conflicts with existing handle for the path segment root in path '" + fullPath + "'")
+			}
+
+			// currently fixed width 1 for '/'
+			i--
+			if path[i] != slashByte {
+				panic("no / before catch-all in path '" + fullPath + "'")
+			}
+
+			n.path = path[offset:i]
+			// first node: catchAll node with empty path
+			child := &node{
+				wildChild: true,
+				nType:     matchesAny,
+			}
+			n.children = []*node{child}
+			n.indices = string(path[i])
+			n = child
+			n.priority++
+			// second node: node holding the variable
+			child = &node{
+				path:     path[i:],
+				nType:    matchesAny,
+				handler:  handler,
+				priority: 1,
+			}
+			n.children = []*node{child}
+			return
+		}
+	}
+
+	if n.nType == hasParams {
+		existing.check(path[offset:], fullPath)
+	}
+
+	// insert remaining path part and handle to the leaf
+	n.path = path[offset:]
+	n.handler = handler
+}
