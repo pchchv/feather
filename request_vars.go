eef32a3f3d6d777bdffd991fa5229c046f454015
pchchv <ipchchv@gmail.com>
2025-04-30 13:26:42 +0300

    Add requestVars type

--- request_vars.go
+++ request_vars.go
@@ -1,6 +1,14 @@
 package feather
 
+import "context"
+
 // ReqVars is the interface of request scoped variables tracked by feather.
 type ReqVars interface {
 	URLParam(pname string) string
+}
+
+type requestVars struct {
+	ctx        context.Context // holds a copy of parent requestVars
+	params     urlParams
+	formParsed bool
 }
