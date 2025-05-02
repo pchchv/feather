4b3fb6c388a964b56c7d4df1525fd66139bf772a
pchchv <ipchchv@gmail.com>
2025-05-02 08:50:06 +0300

    Add gzipWriter type

--- helpers_test.go
+++ helpers_test.go
@@ -1,11 +1,26 @@
 package feather
 
 import (
+	"compress/gzip"
+	"io"
 	"net/http"
+	"sync"
 	"testing"
 
 	. "github.com/pchchv/feather/assert"
 )
+
+var gzipPool = sync.Pool{
+	New: func() interface{} {
+		return &gzipWriter{Writer: gzip.NewWriter(io.Discard)}
+	},
+}
+
+type gzipWriter struct {
+	io.Writer
+	http.ResponseWriter
+	sniffComplete bool
+}
 
 func TestNoRequestVars(t *testing.T) {
 	reqVars := func(w http.ResponseWriter, r *http.Request) {
