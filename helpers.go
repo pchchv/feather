f23ff7fc6f788782868b53f9b41c34d7dd752862
pchchv <ipchchv@gmail.com>
2025-05-02 08:37:17 +0300

    Add RequestVars function to retrieve request scoped variables

---
+++ helpers.go
@@ -0,0 +1,13 @@
+package feather
+
+import "net/http"
+
+// RequestVars returns the request scoped variables tracked by feather.
+func RequestVars(r *http.Request) ReqVars {
+	rv := r.Context().Value(defaultContextIdentifier)
+	if rv == nil {
+		return new(requestVars)
+	}
+
+	return rv.(*requestVars)
+}
