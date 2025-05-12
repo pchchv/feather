cba876888fabe3e4e6d616405d8b06cf5cfabcb0
pchchv <ipchchv@gmail.com>
2025-05-12 10:55:51 +0300

    Refactoring

--- helpers.go
+++ helpers.go
@@ -71,32 +71,6 @@ func ParseMultipartForm(r *http.Request, maxMemory int64) error {
 // Attachment is a helper method for returning an attachement file to be downloaded,
 // if a line needs to be opened, see the Inline function.
 func Attachment(w http.ResponseWriter, r io.Reader, filename string) (err error) {
-	return attachment(w, r, filename)
-}
-
-// AcceptedLanguages returns an array of accepted languages denoted by
-// the Accept-Language header sent by the browser.
-func AcceptedLanguages(r *http.Request) (languages []string) {
-	return acceptedLanguages(r)
-}
-
-func detectContentType(filename string) string {
-	ext := strings.ToLower(filepath.Ext(filename))
-	if t := mime.TypeByExtension(ext); t != "" {
-		return t
-	}
-
-	switch ext {
-	case ".md":
-		return textMarkdown
-	default:
-		return applicationOctetStream
-	}
-}
-
-// attachment is a helper method for returning an attachment file to be downloaded,
-// if a line needs to be opened, see the Inline function.
-func attachment(w http.ResponseWriter, r io.Reader, filename string) (err error) {
 	w.Header().Set(contentDispositionHeader, "attachment;filename="+filename)
 	w.Header().Set(contentTypeHeader, detectContentType(filename))
 	w.WriteHeader(http.StatusOK)
@@ -104,8 +78,9 @@ func attachment(w http.ResponseWriter, r io.Reader, filename string) (err error)
 	return
 }
 
-// acceptedLanguages returns an array of accepted languages denoted by the Accept-Language header sent by the browser.
-func acceptedLanguages(r *http.Request) (languages []string) {
+// AcceptedLanguages returns an array of accepted languages denoted by
+// the Accept-Language header sent by the browser.
+func AcceptedLanguages(r *http.Request) (languages []string) {
 	accepted := r.Header.Get(acceptedLanguageHeader)
 	if accepted == "" {
 		return
@@ -120,4 +95,18 @@ func acceptedLanguages(r *http.Request) (languages []string) {
 	}
 
 	return
+}
+
+func detectContentType(filename string) string {
+	ext := strings.ToLower(filepath.Ext(filename))
+	if t := mime.TypeByExtension(ext); t != "" {
+		return t
+	}
+
+	switch ext {
+	case ".md":
+		return textMarkdown
+	default:
+		return applicationOctetStream
+	}
 }
