30b9ca2038a5482e97a88a4ad9ab6d829f9ebf96
pchchv <ipchchv@gmail.com>
2025-05-13 13:04:29 +0300

    Add a FormDecoder type

--- form.go
+++ form.go
@@ -6,10 +6,20 @@ import (
 	"github.com/pchchv/form"
 )
 
-// FormEncoder is the type used for encoding form data.
+// FormDecoder is the type used for decoding a form for use
+type FormDecoder interface {
+	Decode(interface{}, url.Values) error
+}
+
+// FormEncoder is the type used for encoding form data
 type FormEncoder interface {
 	Encode(interface{}) (url.Values, error)
 }
 
-// DefaultFormEncoder of this package, which is configurable.
-var DefaultFormEncoder FormEncoder = form.NewEncoder()
+var (
+	// DefaultFormDecoder of this package, which is configurable
+	DefaultFormDecoder FormDecoder = form.NewDecoder()
+
+	// DefaultFormEncoder of this package, which is configurable
+	DefaultFormEncoder FormEncoder = form.NewEncoder()
+)
