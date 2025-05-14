fc4cd309208a784e909caa1afa11a41348445d70
pchchv <ipchchv@gmail.com>
2025-05-14 12:40:14 +0300

    Add constructor for closeNotifyingRecorder

--- gzip/gzip_test.go
+++ gzip/gzip_test.go
@@ -6,3 +6,10 @@ type closeNotifyingRecorder struct {
 	*httptest.ResponseRecorder
 	closed chan bool
 }
+
+func newCloseNotifyingRecorder() *closeNotifyingRecorder {
+	return &closeNotifyingRecorder{
+		httptest.NewRecorder(),
+		make(chan bool, 1),
+	}
+}
