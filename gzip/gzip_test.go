8439f044385d39efd0d8138a60c51b7fa3b5eece
pchchv <ipchchv@gmail.com>
2025-05-14 12:42:55 +0300

    Add tests for gzipWriter Flush and Hijack methods

--- gzip/gzip_test.go
+++ gzip/gzip_test.go
@@ -2,8 +2,13 @@ package gzip
 
 import (
 	"bufio"
+	"bytes"
+	"compress/gzip"
 	"net"
 	"net/http/httptest"
+	"testing"
+
+	"github.com/pchchv/feather/assert"
 )
 
 type closeNotifyingRecorder struct {
@@ -30,4 +35,39 @@ func (c *closeNotifyingRecorder) Close() {
 
 func (c *closeNotifyingRecorder) CloseNotify() <-chan bool {
 	return c.closed
+}
+
+func TestGzipFlush(t *testing.T) {
+	rec := httptest.NewRecorder()
+	buff := new(bytes.Buffer)
+	w := gzip.NewWriter(buff)
+	gw := gzipWriter{Writer: w, ResponseWriter: rec}
+	assert.Equal(t, buff.Len(), 0)
+
+	err := gw.Flush()
+	assert.Equal(t, err, nil)
+
+	n1 := buff.Len()
+	assert.NotEqual(t, n1, 0)
+
+	_, err = gw.Write([]byte("x"))
+	assert.Equal(t, err, nil)
+
+	n2 := buff.Len()
+	assert.Equal(t, n1, n2)
+
+	err = gw.Flush()
+	assert.Equal(t, err, nil)
+	assert.NotEqual(t, n2, buff.Len())
+}
+
+func TestGzipHijack(t *testing.T) {
+	rec := newCloseNotifyingRecorder()
+	buf := new(bytes.Buffer)
+	w := gzip.NewWriter(buf)
+	gw := gzipWriter{Writer: w, ResponseWriter: rec}
+	_, bufrw, err := gw.Hijack()
+	assert.Equal(t, err, nil)
+
+	_, _ = bufrw.WriteString("test")
 }
