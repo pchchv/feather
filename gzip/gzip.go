ced4b37c778f404b426c3df923b53bb9d2549021
pchchv <ipchchv@gmail.com>
2025-05-14 12:49:32 +0300

    Implement Gzip middleware for HTTP response compression

--- gzip/gzip.go
+++ gzip/gzip.go
@@ -6,7 +6,15 @@ import (
 	"io"
 	"net"
 	"net/http"
+	"strings"
 	"sync"
+)
+
+const (
+	contentEncodingHeader = "Content-Encoding"
+	acceptEncodingHeader  = "Accept-Encoding"
+	varyHeader            = "Vary"
+	gzipVal               = "gzip"
 )
 
 var gzipPool = sync.Pool{
@@ -39,4 +47,33 @@ func (w *gzipWriter) Write(b []byte) (int, error) {
 	}
 
 	return w.Writer.Write(b)
+}
+
+// Gzip returns a middleware which compresses HTTP response using gzip compression scheme.
+func Gzip(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add(varyHeader, acceptEncodingHeader)
+		if strings.Contains(r.Header.Get(acceptEncodingHeader), gzipVal) {
+			gz := gzipPool.Get().(*gzipWriter)
+			gz.sniffComplete = false
+			gzr := gz.Writer.(*gzip.Writer)
+			gzr.Reset(w)
+			gz.ResponseWriter = w
+			w.Header().Set(contentEncodingHeader, gzipVal)
+			w = gz
+			defer func() {
+				if !gz.sniffComplete {
+					// it is necessary to reset response to its
+					// pristine state where nothing is written to the body
+					w.Header().Del(contentEncodingHeader)
+					gzr.Reset(io.Discard)
+				}
+
+				gzr.Close()
+				gzipPool.Put(gz)
+			}()
+		}
+
+		next(w, r)
+	}
 }
