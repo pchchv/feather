b1a6ae19a8d80ec4e7a4fec5934007f063c9aff7
pchchv <ipchchv@gmail.com>
2025-05-14 12:41:00 +0300

    Implement a Hijack method for closeNotifyingRecorder

--- gzip/gzip_test.go
+++ gzip/gzip_test.go
@@ -1,6 +1,10 @@
 package gzip
 
-import "net/http/httptest"
+import (
+	"bufio"
+	"net"
+	"net/http/httptest"
+)
 
 type closeNotifyingRecorder struct {
 	*httptest.ResponseRecorder
@@ -12,4 +16,10 @@ func newCloseNotifyingRecorder() *closeNotifyingRecorder {
 		httptest.NewRecorder(),
 		make(chan bool, 1),
 	}
+}
+
+func (c *closeNotifyingRecorder) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	reader := bufio.NewReader(c.Body)
+	writer := bufio.NewWriter(c.Body)
+	return nil, bufio.NewReadWriter(reader, writer), nil
 }
